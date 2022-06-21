e1ccd76e63017e6564f9a068605fa80ba82ffa1b
vikrantch-hk <chudhry.vikrant@brightlifecare.com>
2022-06-21 22:40:20 +0530

    Update ReverseString.java

--- src/cracking/code/interview/ReverseString.java
+++ src/cracking/code/interview/ReverseString.java
@@ -17,7 +17,23 @@ public class ReverseString {
 		System.out.println(new String(arr));
 	}
 
+	public static void reverseWithBitwise(String str)
+	{
+		char[] arr = str.toCharArray();
+		int l = 0, r= arr.length-1;
+		while(l<r)
+		{
+			arr[l]= (char)(arr[l]^arr[r]);
+			arr[r]=(char)(arr[l]^arr[r]);
+			arr[l]= (char)(arr[l]^arr[r]);
+			l++;
+			r--;
+		}
+		System.out.println(new String(arr));
+	}
+
 	public static void main(String[] args) {
-		reverse("abcdb");
+		reverse("abcdbe");
+		reverseWithBitwise("abcdbe");
 	}
 }
